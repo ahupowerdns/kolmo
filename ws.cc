086fd101b0ecc36edf09c516993eae813b23e529
bert hubert <bert.hubert@netherlabs.nl>
2017-08-28 12:16:39 +0200

    wip

--- ws.cc
+++ ws.cc
@@ -1,5 +1,50 @@
 #include <crow.h>
 
+
+/* 
+   Welcome to the default free zone!
+   No single number or default can live here EVERYTHING comes from the configuration file.
+
+   The webserver serves URL of SITES which have NAMES and ALTERNATE NAMES.
+   URLs map to DIRECTORIES or ACTIONS (Redirects of various types)
+
+   A SITE listens on one or more IP address LISTENERS
+
+   LISTENERS can have settings too
+
+
+   Settings: 
+
+   GLOBAL
+       Syslog on/off
+
+   LISTENER
+       Address
+       Port
+       Listen-limit
+       Free-bind / non-local
+
+   SITE
+       Domain Name
+       Alternate name
+       Make-canonical 
+       Mapping / -> 
+            DIRECTORY 
+               Path /var/www/
+               Directory-listing: no
+       Mapping /new/ -> 
+            ACTION
+               Target https://new.site/
+               Code 302
+
+       Password /data/ -> Passwords: default
+
+       PASSWORDS
+            Name: default  
+            Entries: [{User: user, Password: password}] 
+
+ */
+
 int main()
 {
   crow::SimpleApp app;
